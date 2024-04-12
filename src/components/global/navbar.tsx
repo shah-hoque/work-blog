71916280470c81baf8d85b9dc0c56404a222b776
shah_hoque1 <shah.hoque10@gmail.com>
2024-04-12 10:21:16 +0100

    app created

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-16">
+        <div className="flex justify-between items-center w-full">
+          <Link href="/">
+            <h1 className="text-2xl font-medium">
+              Shahs <span className="text-teal-500">Blog</span>
+            </h1>
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  );
+}
