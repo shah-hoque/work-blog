e36d4af4cd0f4ffa32ba2b6e350ac47bf86d2b47
shah_hoque1 <shah.hoque10@gmail.com>
2024-04-14 12:06:39 +0100

    ui added

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -9,7 +9,7 @@ export default function navbar() {
       <div className="flex justify-between items-center w-full">
         <Link href="/">
           <h1 className="text-2xl font-medium">
-            Jan <span className="text-teal-500">Blog</span>
+          Shah&apos;s <span className="text-teal-500">Blog</span>
           </h1>
         </Link>
 
